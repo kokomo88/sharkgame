ab6c5775f8140b9d06075cc2040c505613491964
kokomo88 <wcpapir10@freemail.hu>
2015-07-30 10:28:31 +0200

    hehe prealpha :D

--- game.js
+++ game.js
@@ -7,16 +7,47 @@ var mapY = 600;
 var bugSize = 50;
 var bugSpace = 25;
 var bugsInARow = 6;
+var bugMoveFrequency= 800;
+var bugNumber = 12;
 var sharkSizeX = 50;
 var sharkSizeY = 100;
+var projectileSizeX = 3;
+var projectileSizeY = 50 ;
 var bugDirection=0; // 0 means right, 1 left
 var turn = false;
 var endGame = false;
 var time = 0;
 var lastRefresh = 0;
+var lastBugRefresh = 0;
 var previousTimestamp = 0;
 var currentTimestamp = 0 ;
 var elapsedTime = 0;
+var count = 0;
+
+var Key = {
+  _pressed: {},
+
+  LEFT: 37,
+  //UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+	//SPACE: 32,
+
+  isDown: function(keyCode) {
+    return this._pressed[keyCode];
+  },
+
+  onKeydown: function(event) {
+    this._pressed[event.keyCode] = new Date().getTime();
+  },
+
+  onKeyup: function(event) {
+    delete this._pressed[event.keyCode];
+  },
+	//onKeypress: function (event){
+	//	this._pressed[event.keyCode] = new Date().getTime();
+	//}
+};
 
 if (!window.requestAnimationFrame)
 {
@@ -32,7 +63,6 @@ if (!window.requestAnimationFrame)
 		};
 	})();
 }
-
 
 //models
 function Point(x,y){
@@ -52,18 +82,31 @@ function Bug(){
   this.elem = document.createElement("div");
   this.elem.className = "bug";
   this.style=this.elem.style;
+	this.parent=gameContainer;
+	this.destroy = destroyBug;
 }
 function projectile(speed){
   this.position = new Point();
-  this.speed = speed;
+  this.speed = (speed)?speed:1;
   this.elem = document.createElement("div");
   this.elem.className = "projectile";
-    this.style=this.elem.style;
+  this.style=this.elem.style;
+	this.destroy = destroyProjectile;
+	this.parent=gameContainer;
+	this.id = count++;
+	this.elem.id= this.id;
 }
 //modell methods
 function setPoint(x,y){
   this.x=x;
   this.y=y;
+}
+function destroyBug(){
+	this.parent.removeChild(this.elem);
+}
+function destroyProjectile(){
+	//if (!this) return;
+  this.parent.removeChild(this.elem);
 }
 function fire(){
   var p = new projectile();
@@ -78,6 +121,7 @@ function fillBugsArray(n){
     bugs[i]=new Bug();
   }
 }
+
 function initBugsPosition(){
   for (var i in bugs){
     bugs[i].position.setPoint((i%bugsInARow)*(bugSize+bugSpace),Math.floor(i/bugsInARow)*(bugSize+bugSpace));
@@ -85,31 +129,38 @@ function initBugsPosition(){
     gameContainer.appendChild(bugs[i].elem);
   }
 }
+
 function initSharkPosition(){
   laserShark.position.x = (mapX - sharkSizeX)/2  ;
   laserShark.position.y = mapY - sharkSizeY;
   gameContainer.appendChild(laserShark.elem);
 }
+
 function init(){
   time = 0;
   lastRefresh = 0;
   endGame = false;
   gameContainer = document.getElementById("gameContainer");
-  fillBugsArray(12);
+  fillBugsArray(bugNumber);
   laserShark = new Shark();
   initBugsPosition();
   initSharkPosition();
-
 }
-function moveProjectiles(){
+function moveProjectiles(time){
   for (var i in projectiles){
-      projectiles[i].position.y-=25;
+    projectiles[i].position.y-=time * projectiles[i].speed;
+		if (projectiles[i].position.y< (0 - 2 * projectileSizeY)){
+			projectiles[i].destroy();
+			projectiles.splice(i,1);
+		}
   }
 }
-function moveShark(move){
-  laserShark.position.x += move;
-  if (laserShark.position.x < 0 || laserShark.position.x> (mapX-sharkSizeX)){  laserShark.position.x -= move;}
+
+function moveShark(move,time){
+  laserShark.position.x += move*time;
+  if (laserShark.position.x < 0 || laserShark.position.x> (mapX-sharkSizeX)){  laserShark.position.x -= move*time;}
 }
+
 function moveBugs(){
   if (!turn){
     for (var i in bugs){
@@ -141,19 +192,22 @@ function moveBugs(){
 function gameLoop (){
   if (!endGame){
     requestAnimationFrame(gameLoop);
-  //  moveShark();
     previousTimestamp = currentTimestamp;
     currentTimestamp = new Date().getTime();
     elapsedTime += currentTimestamp - previousTimestamp;
-
-    lastRefresh += currentTimestamp - previousTimestamp;
-    if (lastRefresh > 800){
-      lastRefresh = 0;
+		lastRefresh = currentTimestamp - previousTimestamp;
+    lastBugRefresh += currentTimestamp - previousTimestamp;
+    if (lastBugRefresh > bugMoveFrequency){
+      lastBugRefresh = 0;
       moveBugs();
     }
-    moveProjectiles();
-    refreshPosition();
+    moveProjectiles(lastRefresh);
+    refreshPosition(lastRefresh);
     //window.setTimeout(gameLoop, 20);
+		if (Key.isDown(Key.LEFT)) moveShark(-0.5,lastRefresh);
+		if (Key.isDown(Key.RIGHT)) moveShark(+0.5,lastRefresh);
+		detectCollision();
+		if (bugNumber==0)endGame=true;
   }
 }
 function refreshPosition(){
@@ -169,33 +223,50 @@ function refreshElementPosition(e){
   e.style.left = e.position.x;
   e.style.top = e.position.y;
 }
+function detectCollision(){
+	for (var i in projectiles){
+		for (var j in bugs){
+			square = bugs[j];
+			squareSize = bugSize;
+			rect = projectiles[i];
+			rectWidth = projectileSizeX;
+			rectHeight = projectileSizeY;
+
+			if (square.position.x < rect.position.x + rectWidth &&
+		   square.position.x + squareSize > rect.position.x &&
+		   square.position.y < rect.position.y + rectHeight &&
+		   squareSize + square.position.y > rect.position.y) {
+		    // destroyLaserbeam
+				projectiles[i].destroy();
+				projectiles.splice(i,1);
+
+				//destroyBug
+				bugs[j].destroy();
+				bugs.splice(j,1);
+				bugNumber--;
+			}
+		}
+	}
+}
 window.onload= function (){
   init();
   currentTimestamp = new Date().getTime();
   gameLoop();
 };
-document.onkeydown = function(e) {
+document.onkeypress = function(e) {
     e = e || window.event;
     switch(e.which || e.keyCode) {
       case 32: // down
         fire();
         break;
-      case 37: // left
-        moveShark(-10);
-        break;
-
-        case 38: // up
-          fire();
-          break;
-
-        case 39: // right
-          moveShark(10);
-          break;
-
-        case 40: // down
-        break;
+			case	38: // down
+	        fire();
+	        break;
 
         default: return; // exit this handler for other keys
     }
     e.preventDefault(); // prevent the default action (scroll / move caret)
 };
+
+window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
+window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
